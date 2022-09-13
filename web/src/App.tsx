2e862cc779b65dc5186a256c4f3e3e38655a9472
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-09-12 21:16:29 -0300

    first commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,21 @@
+interface ButtonProps{
+  title: string;
+}
+function Button(props: ButtonProps) {
+  return (
+    <button> 
+      {props.title}
+    </button>
+  )
+}
 function App() {
-  return <h1> Hello World! </h1>
+  return (
+    <div>
+    <Button title="send 1" />
+    <Button title="send 2" />
+    <Button title="send 3"/>
+    <Button title="hello"/>
+    </div>
+  )
 }
 export default App
