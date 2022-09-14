e15a7b90c56a0cffbda8393d8f53866eec69fb08
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-09-14 04:46:51 -0300

    salvando

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello React Native</Text>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+
       <StatusBar style="auto" />
     </View>
-  );
+  );}
+
+interface ButtonProps{ 
+title: string;
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffaaad',
     alignItems: 'center',
     justifyContent: 'center',
   },
