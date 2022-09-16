1e2a162b1be4f7051722108824626ed0e29b8516
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-09-16 18:13:48 -0300

    corrigindo erro de tela

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-const app = express();
+
+const app = express()
 //localhost:3333/ads
 
 app.get('/games', (request, response) => {
@@ -7,12 +8,10 @@ app.get('/games', (request, response) => {
 });
 
 app.post('/ads', (request, response) => {
-  return response.status(201).json([]);
+  return response.json([]);
 });
 
-app.get('/games/:id/ads', (request, response) => {
-  const gameId = request.params.id;
-
+app.get('/ads', (request, response) => {
   return response.json([
     { id: 1, name: 'Anuncio 1'},
     { id: 2, name: 'Anuncio 2'},
@@ -22,15 +21,4 @@ app.get('/games/:id/ads', (request, response) => {
   ])
 })
 
-app.get('/ads/:id/discord', (request, response) => {
-  const gameId = request.params.id;
-
-  return response.json([
-    { id: 1, name: 'Anuncio 1'},
-    { id: 2, name: 'Anuncio 2'},
-    { id: 3, name: 'Anuncio 3'},
-    { id: 4, name: 'Anuncio 4'},
-    { id: 5, name: 'Anuncio 5'},
-  ])
-})
 app.listen(3333)
