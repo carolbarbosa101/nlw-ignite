4ed26ceb0178c06bab62fa9137b4077b0526cc48
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-10-01 14:43:55 -0300

    save

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -45,7 +45,7 @@ function App() {
 
           <Dialog.Portal>
           <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px]">
             <Dialog.Title> Publique um anúncio</Dialog.Title>
 
           <Dialog.Content >
