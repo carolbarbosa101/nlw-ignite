14d17e6712718169d6581fc5ae330cb6c54c276a
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-10-14 00:02:56 -0300

    salvando

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,19 @@
+import { useEffect } from 'react';
 import { View, Image, FlatList } from 'react-native';
-
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { GameCard } from '../../components/GameCard';
-
 import { Heading } from '../../components/Heading';
-
 import { GAMES } from '../../utils/games';
 import { styles } from './styles';
+
+
 export function Home() {
+  useEffect(() => {
+    fetch('http://192.168.0.240/games')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, []);
+
   return (
     <View style={styles.container}>
      <Image
