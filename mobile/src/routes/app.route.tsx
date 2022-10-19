70c32ce1bf7289d3c067601eac40b4e3a5ff3a28
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2022-10-18 23:23:48 -0300

    salv

--- mobile/src/routes/app.route.tsx
+++ mobile/src/routes/app.route.tsx
@@ -7,7 +7,7 @@ const { Navigator, Screen} =  createNativeStackNavigator();
 
 export function AppRoutes(){
   return (
-    <Navigator>
+    <Navigator screenOptions={{headerShown: false }}>
       < Screen
         name="home"
         component={Home}
