385f4aea61ae047933a46732133eb20815a1b9f3
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2023-01-11 02:02:00 -0300

    save

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,4 +1,5 @@
-import { Image, TouchableOpacity, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -12,10 +13,13 @@ import { styles } from './styles';
 
 import { GameParams } from '../../@types/@navigation';
 import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+
 
 
 export function Game() {
 
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
   const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
@@ -23,6 +27,13 @@ export function Game() {
   function handleGoBack(){
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`http://192.168.0.240:3333/games/${game.id}/ads`)
+    .then(response => response.json()) 
+    .then(data => setDuos(data));
+  }, []);
+
 
   return (
     <Background>
@@ -38,11 +49,27 @@ export function Game() {
         <Image source={logoImg} style={styles.logo} />
         <View style={styles.right }/>
         </View>
+
+        <Image
+          source={{ uri: game.bannerUrl}}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+    
         <Heading 
           title={game.title}
           subtitle="Conecte-se e comece a jogar!"
         />
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item}/>
+          )}
+        />
 
+     
+        <DuoCard data={duos[0]} />
     </SafeAreaView>
     </Background>
   );
